6020d28b3247b4830c2bbb701ac54651445dd8ff
Jondale <github@jondale.net>
2014-09-16 20:32:03 -0400

    Preset Update

--- kmlaser_presets.py
+++ kmlaser_presets.py
@@ -21,9 +21,9 @@ _presets = {
         "cut": [10000,400,0.4],            
     },
     "6mm-acrylic": {
-        "engrave-light": [10000,1000,0.164],
-        "engrave-heavy": [10000,400,0.152],
-        "cut": [10000,400,0.6],            
+        "engrave-light": [10000,1500,0.2],
+        "engrave-heavy": [10000,1000,0.2],
+        "cut": [10000,200,0.6],            
     },
     "paper": {
         "engrave-light": [1, 10000, 0.2], # Perforated
