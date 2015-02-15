d9666e44c13dc96e0b4b712438e63363eb999af9
Jondale <github@jondale.net>
2015-02-15 16:02:39 -0500

    Update preset values to address tube life decline.

--- extensions/kmlaser_presets.py
+++ extensions/kmlaser_presets.py
@@ -7,33 +7,33 @@ _pcolors = {
 
 _presets = {
     "3mm-wood": {
-        "engrave-light": [10000,400,0.13],
-        "engrave-heavy": [10000,400,0.14],
-        "cut": [10000,400,0.4],
+        "engrave-light": [10000,1000,0.15],
+        "engrave-heavy": [10000,1000,0.2],
+        "cut": [10000,750,0.55],
     },
     "6mm-wood": {
-        "engrave-light": [10000,400,0.13],
-        "engrave-heavy": [10000,400,0.14],
-        "cut": [10000,400,0.6],    
+        "engrave-light": [10000,1000,0.15],
+        "engrave-heavy": [10000,1000,0.2],
+        "cut": [10000,500,0.55],
     },
     "3mm-acrylic": {
-        "engrave-light": [10000,1000,0.164],
-        "engrave-heavy": [10000,400,0.152],
-        "cut": [10000,400,0.4],            
+        "engrave-light": [10000,1000,0.17],
+        "engrave-heavy": [10000,1000,0.25],
+        "cut": [10000,600,0.55],
     },
     "6mm-acrylic": {
         "engrave-light": [10000,1500,0.2],
         "engrave-heavy": [10000,1000,0.2],
-        "cut": [10000,200,0.6],            
+        "cut": [10000,400,0.55],
     },
     "paper": {
         "engrave-light": [1, 1000, 0.2], # Perforated
-        "engrave-heavy": [10000,1500,0.128],
+        "engrave-heavy": [10000,1500,0.15],
         "cut": [10000, 500, 0.165]
     },
     "cardboard": {
-        "engrave-light": [10000,400,0.13],
-        "engrave-heavy": [10000,1000,0.14],
+        "engrave-light": [10000,400,0.15],
+        "engrave-heavy": [10000,1000,0.17],
         "cut": [10000,1000,0.5]
     }
 }
