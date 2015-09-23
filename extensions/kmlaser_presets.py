34a24e28ed591835d6f3bef4b60bed6f83853e16
Jondale <github@jondale.net>
2015-09-23 17:55:58 -0400

    Preset value update due to tube life.

--- extensions/kmlaser_presets.py
+++ extensions/kmlaser_presets.py
@@ -9,22 +9,22 @@ _presets = {
     "3mm-wood": {
         "engrave-light": [10000,1000,0.15],
         "engrave-heavy": [10000,1000,0.2],
-        "cut": [10000,750,0.55],
+        "cut": [10000,400,0.65],
     },
     "6mm-wood": {
         "engrave-light": [10000,1000,0.15],
         "engrave-heavy": [10000,1000,0.2],
-        "cut": [10000,500,0.55],
+        "cut": [10000,400,0.65],
     },
     "3mm-acrylic": {
         "engrave-light": [10000,1000,0.17],
         "engrave-heavy": [10000,1000,0.25],
-        "cut": [10000,600,0.55],
+        "cut": [10000,400,0.65],
     },
     "6mm-acrylic": {
         "engrave-light": [10000,1500,0.2],
         "engrave-heavy": [10000,1000,0.2],
-        "cut": [10000,400,0.55],
+        "cut": [10000,400,0.65],
     },
     "paper": {
         "engrave-light": [1, 1000, 0.2], # Perforated
